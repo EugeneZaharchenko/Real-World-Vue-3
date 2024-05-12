a22727e34326508c2bad2d7bfb355a79b8edaea8
eugene_zaharch <eugenezaharchenko@gmail.com>
2024-05-12 18:55:51 +0300

    program routing

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,6 +1,16 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
-defineProps(['event'])
+import { useRouter } from 'vue-router'
+const router = useRouter()
+// const props = defineProps(['event'])
+
+const register = () => {
+  router.push({
+    name: 'EventDetails',
+  })
+}
 </script>
 <template>
   <p>Register for the event here</p>
+  <button @click="register">Register me</button>
 </template>
